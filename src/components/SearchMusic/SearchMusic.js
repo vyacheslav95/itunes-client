2af52965eeda9cef938ea501a0e8a1c35f33a094
Viacheslav Zaitsev <viacheslav.zajcev@gmail.com>
2021-05-22 16:30:34 +0300

    Cosmetic corrections

--- src/components/SearchMusic/SearchMusic.js
+++ src/components/SearchMusic/SearchMusic.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import classes from './SearchMusic.module.css'
 
-const SearchMusic = props => {
+const SearchMusic = ({
+                       onSearchChange,
+                       onSearchSubmit,
+                     }) => {
 
   // to define the content of the button
   return (
     <div className={classes.SearchMusic}>
       <form
-        onSubmit={props.onSearchSubmit}
+        onSubmit={onSearchSubmit}
       >
         <input
           type="text"
           placeholder={'Artist, Track, Album...'}
-          onChange={event => props.onSearchChange(event.target.value)}
+          onChange={event => onSearchChange(event.target.value)}
         />
         <button type={'submit'}>
           <i className={'fa fa-search'}></i>
